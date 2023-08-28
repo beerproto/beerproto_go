99d651a077eec4aa8c1d06607e4394e7b1de38fc
Ross Merrigan <email@rossmerrigan.com>
2023-08-28 21:07:50 +0200

    Update measureable_units.go

--- measureable_units.go
+++ measureable_units.go
@@ -2,97 +2,97 @@ package beerprotov1
 
 import "google.golang.org/protobuf/proto"
 
-func (x VolumeUnit) DisplayName() string {
+func (x VolumeUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x MassUnit) DisplayName() string {
+func (x MassUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x DiastaticPowerUnit) DisplayName() string {
+func (x DiastaticPowerUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x TemperatureUnit) DisplayName() string {
+func (x TemperatureUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x AcidityUnit) DisplayName() string {
+func (x AcidityUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x TimeUnit) DisplayName() string {
+func (x TimeUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x ColorUnit) DisplayName() string {
+func (x ColorUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x CarbonationUnit) DisplayName() string {
+func (x CarbonationUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x BitternessUnit) DisplayName() string {
+func (x BitternessUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x GravityUnit) DisplayName() string {
+func (x GravityUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x SpecificHeatUnit) DisplayName() string {
+func (x SpecificHeatUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x ConcentrationUnit) DisplayName() string {
+func (x ConcentrationUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x SpecificVolumeUnit) DisplayName() string {
+func (x SpecificVolumeUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x UnitUnit) DisplayName() string {
+func (x UnitUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x PercentUnit) DisplayName() string {
+func (x PercentUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
 }
 
-func (x ViscosityUnit) DisplayName() string {
+func (x ViscosityUnit) StringName() string {
 	test := x.Descriptor().Values().ByNumber(x.Number()).Options()
 	tt := proto.GetExtension(test, E_StringName)
 	return tt.(string)
